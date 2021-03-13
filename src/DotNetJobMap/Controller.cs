dcfa9c7be6f989addf1b941ea1c62204d58cc8f8
Justin Welsch <j.welsch@gmail.com>
2021-03-12 21:37:18 -0500

    Remove old code

--- src/DotNetJobMap/Controller.cs
+++ src/DotNetJobMap/Controller.cs
@@ -9,22 +9,12 @@ namespace DotNetJobMap
 
     public class Controller : IController
     {
-        private readonly IJobManager _jobManager;
-        private readonly IMessageRouter _messageRouter;
-        private readonly IMessageQueue _messageQueue;
-
-        public Controller(IJobManager jobManager, IMessageRouter messageRouter, IMessageQueue messageQueue)
+        public Controller()
         {
-            _jobManager = jobManager;
-            _messageRouter = messageRouter;
-            _messageQueue = messageQueue;
         }
 
         public void DoNext()
         {
-            // _messageQueue.TryDequeue(out IMessageEnvelope envelope);
-            // var job = _messageRouter.Route(envelope.Address);
-            // job.Execute();
         }
     }
 }
